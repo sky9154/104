2448cbf7fb5100a66d3ba0fae703dbc6df54e7de
oF <sky0932039612@gmail.com>
2023-07-14 17:16:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from datetime import datetime
-from time import strftime
+from time import localtime, sleep, strftime
 from toast import toast
 from requests import request
 from os import getenv
@@ -18,44 +18,59 @@ END_DATE = getenv('END_DATE')
 
 card_data = None
 
-if AUTO in ['TRUE', 'True', 'true', 't']:
-  begin_day, end_day = get.begin_end_day()
+def main (card_data: list):
+  if card_data:
+    url = 'https://apis.104api.com.tw/prohrm/1.0/hrmapi/external/transferCard'
 
-  card_data = get.card_data(begin_day, end_day)
+    payload = dumps(card_data)
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': f'Bearer {get.access_token(CLIENT_ID, CLIENT_SECRET)}'
+    }
+
+    response = request('POST', url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+      date = strftime('%Y%m%d_%H%M%S')
+
+      with open(f'log/user/{date}.log', 'w+', newline='', encoding='utf-8') as txt:
+        for data in card_data:
+          message = f"員工編號: {data['empNo']}\n打卡時間: {datetime.fromtimestamp (data['cardTime'] / 1000)}\n"
+
+          print(message, file=txt)
+      
+      with open(f'log/system/{date}.log', 'w+', newline='') as txt:
+        response_dict = response.json()
+        response_json = dumps(response_dict, indent=4)
+
+        print(response_json, file=txt)
+
+      toast('上傳完畢')
+  else:
+    toast('未取得打卡資料')
+
+
+if AUTO in ['TRUE', 'True', 'true', 't']:
+  while True:
+    local_time = localtime()
+    time = strftime('%H%M%S', local_time)
+
+    if time in ['100000', '220000']:
+      if __name__ == '__main__':
+        begin_day, end_day = get.begin_end_day()
+
+        card_data = get.card_data(begin_day, end_day)
+
+        main(card_data)
+
+    sleep(1)
 elif AUTO in ['FALSE', 'False', 'false', 'f']:
   if BEGIN_DATE == '' or END_DATE == '':
     toast('需填入開始與結束日期')
   else:
     card_data = get.card_data(BEGIN_DATE, END_DATE)
- 
-if card_data:
-  url = 'https://apis.104api.com.tw/prohrm/1.0/hrmapi/external/transferCard'
 
-  payload = dumps(card_data)
-
-  headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': f'Bearer {get.access_token(CLIENT_ID, CLIENT_SECRET)}'
-  }
-
-  response = request('POST', url, headers=headers, data=payload)
-
-  if response.status_code == 200:
-    date = strftime('%Y%m%d_%H%M%S')
-
-    with open(f'log/user/{date}.log', 'w+', newline='', encoding='utf-8') as txt:
-      for data in card_data:
-        message = f"員工編號: {data['empNo']}\n打卡時間: {datetime.fromtimestamp (data['cardTime'] / 1000)}\n"
-
-        print(message, file=txt)
-    
-    with open(f'log/system/{date}.log', 'w+', newline='') as txt:
-      response_dict = response.json()
-      response_json = dumps(response_dict, indent=4)
-
-      print(response_json, file=txt)
-
-    toast('上傳完畢')
-else:
-  toast('未取得打卡資料')
+    if __name__ == '__main__':
+      main(card_data)
--- sql/select.sql
+++ sql/select.sql
@@ -1,9 +1,17 @@
 SELECT
-	  [Person].[CardNo] AS '員工編號',
-	  [DoorLog].[LogArrivalDateTime] AS '打卡時間'
-FROM [DoorLog], [Person]
-WHERE
-    [DoorLog].[EmployeeID] = [Person].[EmployeeID] and
-    [DoorLog].[EventAlarmCode] = '正常' and
-    [DoorLog].[LogArrivalDateTime] >= '_BEGINDATE_' and
-    [DoorLog].[LogArrivalDateTime] <= '_ENDDATE_'
+    [CardNo] AS '員工編號',
+    [DateTime] AS '打卡時間'
+FROM (
+    SELECT 
+        [Person].[CardNo],
+        [DoorLog].[DateTime],
+        ROW_NUMBER() OVER (PARTITION BY [Person].[CardNo] ORDER BY [Person].[CardNo]) AS row_num
+    FROM [Person], [DoorLog]
+    WHERE
+        [DoorLog].[EmployeeID] = [Person].[EmployeeID] and
+        [DoorLog].[EventAlarmCode] = '正常' and
+        [DoorLog].[DateTime] >= '_BEGINDATE_' and
+        [DoorLog].[DateTime] <= '_ENDDATE_'
+) AS subquery
+WHERE row_num = 1
+ORDER BY [CardNo]
